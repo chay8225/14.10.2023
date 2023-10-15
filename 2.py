9dedf7edd7a9ed12e14780e2f92037454b756bea
alisa_mj4c2qn <gfhff@.com>
2023-10-15 14:57:59 +0300

    version 1

---
+++ 2.py
@@ -0,0 +1,13 @@
+frase = str(input())
+
+a = frase.count('a')
+i = frase.count('i')
+u = frase.count('u')
+o = frase.count('o')
+e = frase.count('e')
+
+print(str(a) + ' used times')
+print(str(i) + ' used times')
+print(str(u) + ' used times')
+print(str(o) + ' used times')
+print(str(e) + ' used times')
---
+++ 3.py
@@ -0,0 +1,8 @@
+n = int(input())
+fc = 1
+while n > 1:
+    fc = fc * n
+    n = n - 1
+
+print(fc)
+
