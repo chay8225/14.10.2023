18deaaffeb367aa6251261ed5913c5b24941d24c
Bltimur <147918853+Bltimur@users.noreply.github.com>
2023-11-10 17:40:47 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,15 @@
-def factorial():
-    n = int(input('Write a  number:'))
-    fc = 1
-    while n > 1:
-        fc = fc * n
-        n = n - 1
+result = []
+def divider(a, b, reise=None):
+  if a < b:
+    raise ValueError
+  if b > 100:
+    reise IndexError
+  return a/b
 
-    print(fc)
-factorial()
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 
+for key in data:
+  res = divider(key, data[Kem])
+  result.append(res)
+
+  print(result)
